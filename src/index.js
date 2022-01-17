1bf25507eb37c72ac6caff22c5ba2820474c0014
Shrey Khosla <theshreykhosla@gmail.com>
2022-01-17 18:17:59 -0500

    Added React Snapshot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Nav from "./Nav/Nav";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { render } from "react-snapshot";
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <BrowserRouter>
       <Nav />
