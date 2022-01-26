7e52fcfc4bc25dc97ee08e6fb13b535996c50f8e
Shrey Khosla <theshreykhosla@gmail.com>
2022-01-26 01:00:06 -0500

    Added roadmap

--- src/App.js
+++ src/App.js
@@ -1,55 +1,146 @@
-import {
-  FaDiscord,
-  FaTiktok,
-  FaTwitter,
-  FaTwitch,
-  FaYoutube,
-} from "react-icons/fa";
 import "./App.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
+import { FaFlagCheckered } from "react-icons/fa";
 const App = (props) => {
   return (
-    <div className="App">
-      Coming Soon
-      <div className="links">
-        <a
-          className="discord"
-          href="https://discord.gg/Xe7bzpBZ"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaDiscord size={50} />
-        </a>
-        <a
-          className="tiktok"
-          href="https://vm.tiktok.com/ZMLe9cony/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaTiktok size={50} />
-        </a>
-        <a
-          className="twitter"
-          href="https://twitter.com/the_blobs_NFT"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaTwitter size={50} />
-        </a>
-        {/* <a href="https://discord.gg/Xe7bzpBZ" target="_blank">
-        rel="noreferrer"
-          <FaTwitch size={50} />
-        </a> */}
-        <a
-          className="youtube"
-          href="https://www.youtube.com/channel/UCA9GZSixmruDvonbVlwQ5wA"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaYoutube size={50} />
-        </a>
-      </div>
-    </div>
+    <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+        date="Phase I"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title">
+          theBlobs have landed
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">
+          Blob #59 - The Money Blob
+        </h4>
+        <h4 className="vertical-timeline-element-subtitle">
+          <img
+            style={{ width: "450px" }}
+            src="https://gateway.pinata.cloud/ipfs/QmPtjaFE64qx9Eu6Lwt5fZ2JaZ8uo8FCXQUaUdNSorwhgj/59.png"
+            alt="theBlob #59"
+          />
+        </h4>
+        <p>
+          100 Blobs have arrived. 1/1 Blobs. Each with their unique traits and
+          abilities. Many more shall come, but these were the first, most
+          courageous Blobs!
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="Phase II"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="Phase III"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date=""
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date=""
+        iconStyle={{ background: "rgb(0,0,0)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="Phase 2D"
+        iconStyle={{ background: "rgb(0,0,0)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date=""
+        iconStyle={{ background: "rgb(0,0,0)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date=""
+        iconStyle={{ background: "rgb(0,0,0)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date=""
+        iconStyle={{ background: "rgb(0,0,0)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="Phase 3D"
+        iconStyle={{ background: "rgb(0,0,0)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date=""
+        iconStyle={{ background: "rgb(0,0,0)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      >
+        <h3 className="vertical-timeline-element-title"></h3>
+        <h4 className="vertical-timeline-element-subtitle"></h4>
+        <p></p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+        icon={<FaFlagCheckered />}
+      />
+    </VerticalTimeline>
   );
 };
 
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,4 +1,11 @@
 import { Link, useLocation } from "react-router-dom";
+import {
+  FaDiscord,
+  FaTiktok,
+  FaTwitter,
+  FaTwitch,
+  FaYoutube,
+} from "react-icons/fa";
 import "./Nav.css";
 
 const Nav = (props) => {
@@ -14,6 +21,42 @@ const Nav = (props) => {
         <Link to="/blobs">theBLOBS</Link>
         <Link to="/about">ABOUT</Link>
         <Link to="/account">ACCOUNT</Link>
+        <a
+          className="discord"
+          href="https://discord.gg/Xe7bzpBZ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaDiscord size={25} />
+        </a>
+        <a
+          className="tiktok"
+          href="https://vm.tiktok.com/ZMLe9cony/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTiktok size={25} />
+        </a>
+        <a
+          className="twitter"
+          href="https://twitter.com/the_blobs_NFT"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTwitter size={25} />
+        </a>
+        {/* <a href="https://discord.gg/Xe7bzpBZ" target="_blank">
+        rel="noreferrer"
+          <FaTwitch size={25} />
+        </a> */}
+        <a
+          className="youtube"
+          href="https://www.youtube.com/channel/UCA9GZSixmruDvonbVlwQ5wA"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaYoutube size={25} />
+        </a>
       </div>
     </nav>
   )
